1afb5822784e966b2bafb20fbae147a013f6e4bd
Venkata Kishore Vusa <venkatakishorevusa@gmail.com>
2025-11-19 19:54:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -9,3 +13,6 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
